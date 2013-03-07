3f86837e9a1b226bfb83a5c38bcd434efa9a64df
TYler <tygibb@gmail.com>
2013-03-07 16:50:50 -0700

    email reg ex

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ActiveRecord::Base
   attr_accessible :address, :comment, :email, :first_name, :last_name, :phone
   validates :first_name, :last_name, :email,  :presence => {:message => 'field can\'t be blank'}
   validates :email, :uniqueness => { :message => 'email is aleady in our records.'}
-  validates :email, :format => { :with => /^.+@.+$/,  :message => "this email is not valid." }
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,  :message => "this email is not valid." }
   validates :phone, :format => { :with =>/^\(\d{3}\)-\d{3}-\d{4}$/, :message=> "invalid format. try (XXX)-XXX-XXXX"}
 
 end
