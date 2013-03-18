bc86c7dcf47d09d7c42465ea7f5f47c8ff42af7a
jp <japr94@hotmail.com>
2013-03-17 19:08:57 -0600

    additional rates > 0

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,7 @@ class Reservation < ActiveRecord::Base
 	belongs_to :customer
 
   attr_accessible :room_id, :customer_id, :date_in, :date_out, :rate_additional, :no_adults, :no_children, :comment
+  validates :rate_additional, :numericality => {:greater_than_or_equal_to => 0}
   validates_date :date_in,  :on_or_after => lambda { Date.current } 
   validates_date :date_out, :after => lambda { :date_in}
   
