4addf554e45cba29ea57b483f5a1cf640164c9b6
jp <japr94@hotmail.com>
2013-03-18 01:23:48 -0600

    test nil in price validator and more tests for reservations

--- app/validators/price_validator.rb
+++ app/validators/price_validator.rb
@@ -1,7 +1,7 @@
 class PriceValidator < ActiveModel::EachValidator
   # validates prices up to $9999.99
   def validate_each(record, attribute, value)
-    if (value < 0.01 || value >= 10000)
+    if (value == nil || value < 0.01 || value >= 10000)
       record.errors[attribute] << "Invalid price, must be greater than or equal to $0.01 and less than $10,000"
     end
   end
--- test/unit/reservation_test.rb
+++ test/unit/reservation_test.rb
@@ -1,13 +1,11 @@
 require 'test_helper'
 
 class ReservationTest < ActiveSupport::TestCase
-=begin
-  def test_should_be_valid
-    assert Reservation.new.valid?
+
+  test "should not save reservation with room conflicts" do
+    
   end
-=end
-	test "should not save reservation with room conflicts" do
-	end
+  
   test "should not save reservation with invalid dates" do
   	@invalid_reservation = Reservation.new(:date_in => "10-10-2033", :date_out => "10-10-2000")
   	assert !@invalid_reservation.save
@@ -19,4 +17,11 @@ class ReservationTest < ActiveSupport::TestCase
   	assert !@invalid_reservation.save
   	assert_not_nil @invalid_reservation.errors[:additional_rate]
   end 
+
+  test "should not allow a registration with not adults" do
+  	@invalid_reservation = Reservation.new(:no_adults => "0", :no_children => "4")
+  	assert !@invalid_reservation.save
+  	assert_not_nil @invalid_reservation.errors[:no_adults]
+  end
+
 end
