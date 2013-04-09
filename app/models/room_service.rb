def708380805ed66504b06ab5f714046d490b9e6
Tyler <tygibb@gmail.com>
2013-04-09 16:48:50 -0600

    Rs vald

--- app/models/room_service.rb
+++ app/models/room_service.rb
@@ -4,4 +4,8 @@ class RoomService < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :hotel
   attr_accessible :customer_id, :hotel_id, :order, :price, :registration_id, :reservation_id, :service_id, :serviced, :room_id
+
+validates :hotel_id, :order, :presence => {:message => 'Field Cannot Be Blank'}
+validates :serviced, :inclusion => { :in => [true, false], :message => 'Field cannot be blank'}
+validates :price, :price => true
 end
