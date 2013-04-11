b350c5cd090ef71d479b78a1e001905e71942eb5
Tyler <tygibb@gmail.com>
2013-04-11 16:35:20 -0600

    stuff can be free

--- app/validators/price_validator.rb
+++ app/validators/price_validator.rb
@@ -1,8 +1,8 @@
 class PriceValidator < ActiveModel::EachValidator
   # validates prices up to $9999.99
   def validate_each(record, attribute, value)
-    if (value != nil  &&( value < 0.01 || value >= 10000))
-      record.errors[attribute] << "Invalid price, must be greater than or equal to $0.01 and less than $10,000"
+    if (value != nil  &&( value < 0.00 || value >= 10000))
+      record.errors[attribute] << "Invalid price, must be greater than or equal to $0.00 and less than $10,000"
     end
   end
 end
