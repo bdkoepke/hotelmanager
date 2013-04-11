b9d776e543656ce981bbfbac6968d2c461c037cc
jp7 <japr94@hotmail.com>
2013-04-11 03:11:05 -0600

    room service to invoice

--- app/admin/invoices.rb
+++ app/admin/invoices.rb
@@ -1,4 +1,19 @@
 ActiveAdmin.register Invoice do
+
+controller do 
+	def computeTotal
+=begin		
+		@services = RoomService.where(:hotel_id => invoice.reservation.hotel.id, :room_id => 			invoice.reservation.room.id)
+		servicesTotal = 0
+		for service in @services
+			servicesTotal += service.price
+		end	
+		reservation.room.price + reservation.rate_additional + servicesTotal	
+=end
+
+	end
+ end
+
   menu :if => proc{ current_admin_user.role == "admin" || current_admin_user.role == "sales associate" }
 
   config.batch_actions = false
@@ -8,7 +23,7 @@ ActiveAdmin.register Invoice do
   scope :clear_balance
   scope :outstanding_balance
 
-index do
+index do 
     column "Hotel" do |invoice|
       invoice.reservation.hotel.name
     end
@@ -20,44 +35,67 @@ index do
       invoice.reservation.room.name
     end
     column "Total" do |invoice|
-      invoice.reservation.room.price + invoice.reservation.rate_additional
+##
+		@services = RoomService.where(:hotel_id => invoice.reservation.hotel.id, :room_id => 			invoice.reservation.room.id)
+		servicesTotal = 0
+		for service in @services
+			servicesTotal += service.price
+		end	
+		if (invoice.reservation.rate_additional == nil)
+			additional = 0
+		else
+			additional = invoice.reservation.rate_additional
+		end
+		invoice.reservation.room.price + additional + servicesTotal	
+
+##
+#  invoice.reservation.room.price + invoice.reservation.rate_additional
     end
     
     default_actions
 end
-
   show do
 	@title = "Invoice - " + invoice.reservation.customer.name
   @status = invoice.reservation.status
   
-  @balance = 0
-  if(@status != "Checked-out")
-    @balance = invoice.reservation.room.price + invoice.reservation.rate_additional
-  end
+  
 
   panel @title do
-
-
+	#@service = 
+	@services = RoomService.where(:hotel_id => invoice.reservation.hotel.id, :room_id => invoice.reservation.room.id)
   attributes_table_for invoice do
 
-   #for invoice.reservation.room_service do |service|
-   #   row service.order do
-   #     service.price
-   #   end
-   #end
-		
+	@service_prices = 0;
+ 	for service in @services
+	@service_prices += service.price
+row service.order do
+			service.price
+		end
+end
+	
     row "Room Price" do
           invoice.reservation.room.price
     end
-		row "Additional Price" do
-          invoice.reservation.rate_additional
-    end
+	row "Additional Price" do
+
+		if (invoice.reservation.rate_additional == nil)
+			@additional = 0
+		else
+          	@additional =invoice.reservation.rate_additional
+		end
+    	end
 	  row  "Total" do
-          invoice.reservation.room.price + invoice.reservation.rate_additional
+	#:computeTotal          
+		
+@total = invoice.reservation.room.price + @additional + @service_prices
     end
     row "Balance" do
-        @balance
-    end
+    if(@status != "Checked-out")
+    	@total
+    else
+        0
+end    
+	end
     end
 end
     active_admin_comments
